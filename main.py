66a87ace5d514c13293599d7b1fb64e36f701ce5
Jayapaveethra <>
2023-01-27 07:39:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Hello World!")
+print("Hi Everyone\nGood Morning")
+print("Hello JP")
+print("Good" +" "+"to"+" "+"you") # +" "+ This will provide spaces between the words
+print("Hello" +" " + input("What is your name?")) #First the script will ask the user to input the name and then it prints with hello and with user input
